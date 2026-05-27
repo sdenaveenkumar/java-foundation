7d7cf27a8d969d9e92cb692079eb73d646078e36
Naveen Kumar <sde.naveenkumar@gmail.com>
2026-05-27 07:54:14 +0530

    add solutiion of leap year

--- src/basics/leap_year.java
+++ src/basics/leap_year.java
@@ -1,4 +1,13 @@
 package basics;
 
 public class leap_year {
+    public static void main(String[] args) {
+        int year = 2026;
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println("leap year");
+        }else {
+            System.out.println("not a leap year");
+        }
+    }
 }
